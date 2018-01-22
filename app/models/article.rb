8c1971b9d55debc31d13803ee20830fc5a02c81f
Jerica Mitchell <jericam32@gmail.com>
2018-01-22 20:27:30 +0000

    added association between user and articles and added debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ActiveRecord::Base
-    
+    belongs_to :user
     validates :title, presence: true, length: { minimum: 8, maximum: 50 }
     validates :description, presence: true, length: { minimum: 10, maximum: 600}
+    validates :user_id, presence: true
     
     
     
