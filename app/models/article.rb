5d9e79f6f0f9248f2a20764897b11a867ac29134
Jerica Mitchell <jericam32@gmail.com>
2018-01-22 18:19:47 +0000

    Styled articles listing template

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
     
-    validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-    validates :description, presence: true, length: { minimum: 10, maximum: 50 }
+    validates :title, presence: true, length: { minimum: 8, maximum: 50 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 600}
     
     
     
