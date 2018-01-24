9e40856be9db29f2e4d249b2fdb596bc2afc6a5f
Jerica Mitchell <jericam32@gmail.com>
2018-01-24 18:10:01 +0000

    CSS changes and Admin User functionality added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    before_action :require_admin, only: [:destroy]
     
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
@@ -24,7 +25,7 @@ class UsersController < ApplicationController
     end
     def update
         if @user.update(user_params)
-            flash[:success] = "You account was successfully updated!"
+            flash[:success] = "Your account was successfully updated!"
             redirect_to articles_path
         else
             render 'new'
@@ -32,6 +33,12 @@ class UsersController < ApplicationController
     end
     def show
         @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    end
+    def destroy
+       @user = User.find(params[:id])
+       @user.destroy
+       flash[:danger] = "User and all of their articles have been deleted"
+       redirect_to users_path
     end
     
     private
@@ -42,9 +49,15 @@ class UsersController < ApplicationController
          @user = User.find(params[:id])
     end
     def require_same_user
-        if current_user != @user
+        if current_user != @user and !current_user.admin?
             flash[:danger] = "You can only edit your own profile!"
             redirect_to root_path
         end
+    end
+    def require_admin
+       if logged_in? and !current_user.admin?
+           flash[:danger] = "Only admin users can perform that action!"
+           redirect_to root_path
+       end
     end
 end
