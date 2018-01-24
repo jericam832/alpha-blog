98536b01100981b23133e1337e359c558c21d5e9
Jerica Mitchell <jericam32@gmail.com>
2018-01-24 15:40:59 +0000

    Authentication system implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:edit, :update, :show, :destroy]
+    before_action :required_user, except: [:index, :show]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
     
     def index
         @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -41,8 +43,6 @@ class ArticlesController < ApplicationController
     end
     
     def destroy
-        
-        
         @article.destroy
         flash[:danger] = "Article was successfully deleted"
         redirect_to articles_path
@@ -55,6 +55,14 @@ class ArticlesController < ApplicationController
     def article_params
         params.require(:article).permit(:title, :description)
     end
+    
+    def require_same_user
+        if current_user != @article.user
+            flash[:danger] = "You can only edit or delete your own article!"
+            redirect_to root_path
+        end
+    end
+
     
     
 end
