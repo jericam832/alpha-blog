f26a435c01676eeae78ea1edacb28d34c16728fb
Jerica Mitchell <jericam32@gmail.com>
2018-01-26 18:48:40 +0000

    added require admin for category create

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -17,7 +17,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
         assert_template 'categories/new'
         assert_no_difference 'Category.count' do
             post categories_path, category: {name: " "}
-    end
+        end
         assert_template 'categories/new'
         assert_select 'h3.panel-title'
         assert_select 'div.panel-body'
